1adbe5632e7c99085ef65ff2cb4c9070fa33d2b2
Katerina <katerina4484@mail.ru>
2022-08-14 21:56:18 +0500

    Задача1

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -33,36 +33,3 @@ int ReadInt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-/* Версия Дениса
-int x = ReadInt("Введите координату X: ");
-int y = ReadInt("Введите координату Y: ");
-
-if(x == 0 || y == 0)
-{
-Console.WriteLine("Четверть определить невозможно");
-return;
-}
-
-if (x > 0 && y > 0)
-{
-Console.WriteLine("Четверть 1");
-}
-else if(x < 0 && y > 0)
-{
-Console.WriteLine("Четверть 2");
-}
-else if(x < 0 && y < 0)
-{
-Console.WriteLine("Четверть 3");
-}
-else if(x > 0 && y < 0)
-{
-Console.WriteLine("Четверть 4");
-}
-
-
-int ReadInt(string message)
-{
-Console.Write(message);
-return Convert.ToInt32(Console.ReadLine());
-}*/
