d207460f7088cac71739011a4ceda5021be185b8
Katerina <katerina4484@mail.ru>
2022-08-15 22:19:18 +0500

    Задача4

---
+++ Задача4/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+
+int number = ReadInt("Введите число: ");
+int count =1;
+
+while(count < number)
+{
+Console.Write(Sqr(count) + ",");
+count++; 
+if (count == number) // избавляемся от точки и запятой в конце
+{
+    Console.Write(Sqr(count) + " ");
+}
+}
+
+
+int ReadInt(string message) // функция
+{
+     Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sqr(int num1)  // функция
+{
+    return num1 * num1;
+}
